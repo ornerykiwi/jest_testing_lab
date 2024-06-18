816923a56cdc6b18cb33e364c7f0ac556728bd7c
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-18 14:29:21 +0100

    add second test for sum

--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -17,7 +17,9 @@ describe('sum', () => {
   });
 
   test('can add two large positive numbers', () => { 
-    
+    expected = 900000000;
+    actual = sum(400000000, 500000000);
+    expect(actual).toBe(expected);
   });
 
   test('can add two negative numbers', () => {
