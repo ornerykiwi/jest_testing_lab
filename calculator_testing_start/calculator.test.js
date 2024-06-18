239a7ea4dbcd31bc5d62381b3e2e0626893dd462
Thomas Peace <thomasjpeace29@gmail.com>
2024-06-18 14:40:16 +0100

    finish multiply testing

--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -38,9 +38,57 @@ describe('sum', () => {
 
 describe('subtract', () => {
 
+  test('can subtract two small numbers', () => {
+    expected = 5;
+    actual = subtract(15, 10);
+    expect(actual).toBe(expected);
+  })
+
+  test('can subtract two large numbers', () => {
+    expected = 100;
+    actual = subtract(1000000000, 999999900);
+    expect(actual).toBe(expected);
+  })
+
+  test('can subtract two negative numbers', () => {
+    expected = -29;
+    actual = subtract(-37, -8);
+    expect(actual).toBe(expected);
+  })
+
+  test('can subtract zero', () => {
+    expected = 5;
+    actual = subtract(5, 0);
+    expect(actual).toBe(expected);
+  })
+
 });
 
 describe('multiply', () => {
+
+  test('can multiply 2 small numbers', () => {
+    expected = 45;
+    actual = multiply(9, 5);
+    expect(actual).toBe(expected);
+  })
+
+  test('can multiply 2 large numbers', () => {
+    expected = 900000000;
+    actual = multiply(30000, 30000);
+    expect(actual).toBe(expected);
+  })
+
+  test('can multiply 2 negative numbers', () => {
+    expected = 450;
+    actual = multiply(-90, -5);
+    expect(actual).toBe(expected);
+  })
+
+  test('can multiply by zero', () => {
+    expected = 0;
+    actual = multiply(12345, 0);
+    expect(actual).toBe(expected);
+  })
 
 });
 
